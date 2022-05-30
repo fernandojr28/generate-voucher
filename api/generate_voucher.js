f436d8e0997933b1b431e95a19650935a656dbfd
fernandojr28 <fernandojr280897@gmail.com>
2022-05-30 03:25:06 -0500

    upgrade

--- api/generate_voucher.js
+++ api/generate_voucher.js
@@ -8,7 +8,7 @@ router.use('/generate-voucher', async (req, res) => {
   // const data = await gmail.readInboxContent(process.env.READ_INBOX_GMAIL)
   const data = await gmail.getAllData(req, process.env.READ_INBOX_GMAIL)
   // const data2 = await gmail.getAllData(process.env.READ_INBOX_GMAIL)
-  res.json({ data })
+  res.json(data)
   res.end()
 })
 
